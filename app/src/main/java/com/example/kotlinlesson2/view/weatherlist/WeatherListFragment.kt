d60e0e848f58a6fa044fb15d6935c982ecec96bb
satsura88 <satsura88@gmail.com>
2022-07-02 22:46:38 +0300

    апргрейт решения проблемы утечки памяти в WeatherListFragment (binding)

--- app/src/main/java/com/example/kotlinlesson2/view/weatherlist/WeatherListFragment.kt
+++ app/src/main/java/com/example/kotlinlesson2/view/weatherlist/WeatherListFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -19,20 +18,26 @@ class WeatherListFragment : Fragment() {
         fun newInstance() = WeatherListFragment()
     }
 
-    private var binding: FragmentWeatherListBinding?= null
+    private var _binding: FragmentWeatherListBinding?= null
+    private val binding: FragmentWeatherListBinding
+
+        get(){
+            return _binding!!
+        }
 
     override fun onDestroy() {
         super.onDestroy()
-        binding = null
+        _binding = null
     }
+
     lateinit var viewModel: WeatherListViewModel
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentWeatherListBinding.inflate(inflater)
-        return binding!!.root
+        _binding = FragmentWeatherListBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -49,20 +54,20 @@ class WeatherListFragment : Fragment() {
     private fun renderData(appState: AppState){
         when(appState){
             is AppState.Error -> {
-                binding!!.loadingLayout.visibility = View.GONE
-                Snackbar.make(binding!!.mainView, "ERROR", Snackbar.LENGTH_LONG).setAction("Restart"){
+                binding.loadingLayout.visibility = View.GONE
+                Snackbar.make(binding.mainView, "ERROR", Snackbar.LENGTH_LONG).setAction("Restart"){
                     viewModel.repository.getListWeather()
                 }.show()
             }
             AppState.Loading -> {
-                 binding!!.loadingLayout.visibility = View.VISIBLE
+                 binding.loadingLayout.visibility = View.VISIBLE
             }
             is AppState.Success -> {
                 val result = appState.weatherData
-                binding!!.cityName.text = result.city.name
-                binding!!.temperatureValue.text = result.temperature.toString()
-                binding!!.feelsLikeValue.text = result.feelsLike.toString()
-                binding!!.cityCoordinates.text = "${result.city.lat}/${result.city.lon}"
+                binding.cityName.text = result.city.name
+                binding.temperatureValue.text = result.temperature.toString()
+                binding.feelsLikeValue.text = result.feelsLike.toString()
+                binding.cityCoordinates.text = "${result.city.lat}/${result.city.lon}"
             }
         }
     }
